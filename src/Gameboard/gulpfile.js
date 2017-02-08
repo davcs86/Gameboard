3baa978fe5297a86747391c15eacbc5483115765
David Castillo <davcs86@gmail.com>
2017-02-07 18:08:01 -0600

    Set up of webpack2 as module bundler

---
+++ src/Gameboard/gulpfile.js
@@ -0,0 +1,25 @@
+﻿
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var webpack = require("webpack");
+var webpackConfig = require("./webpack.config.js");
+
+var webpackCB = function(cb) {
+    return function(err, stats) {
+        if (err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString());
+        cb();
+    };
+};
+
+gulp.task("default", function (callback) {
+    // run webpack
+    webpack(webpackConfig(true, true), webpackCB(callback));
+    webpack(webpackConfig(false, true), webpackCB(callback));
+});
+
+gulp.task("no-lint", function (callback) {
+    // run webpack w/ linting
+    webpack(webpackConfig(true, false), webpackCB(callback));
+    webpack(webpackConfig(false, false), webpackCB(callback));
+});
