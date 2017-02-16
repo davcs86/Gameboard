7908cf594e49256e745d5e9b853ccbfb941ccd3c
David Castillo <davcs86@gmail.com>
2017-02-16 16:19:21 -0600

    Fix error in the minification of the angular module.

--- src/Gameboard/App/sections/companies/companies.module.js
+++ src/Gameboard/App/sections/companies/companies.module.js
@@ -4,14 +4,7 @@ import Angular from "angular";
 import Routes from "./companies.routes";
 import CompaniesService from "./companies.service";
 
-import ListCtrl from "./list/list.controller";
-import EditCtrl from "./edit/edit.controller";
-import NewCtrl from "./new/new.controller";
-
 export default Angular.module("davcs86_gameboard_companies", [])
     .service(CompaniesService.name, CompaniesService)
-    .controller(ListCtrl.name, ListCtrl)
-    .controller(EditCtrl.name, EditCtrl)
-    .controller(NewCtrl.name, NewCtrl)
     .config(Routes)
     .name;
--- src/Gameboard/App/sections/products/products.module.js
+++ src/Gameboard/App/sections/products/products.module.js
@@ -4,14 +4,7 @@ import Angular from "angular";
 import Routes from "./products.routes";
 import ProductsService from "./products.service";
 
-import ListCtrl from "./list/list.controller";
-import EditCtrl from "./edit/edit.controller";
-import NewCtrl from "./new/new.controller";
-
 export default Angular.module("davcs86_gameboard_products", [])
     .service(ProductsService.name, ProductsService)
-    .controller(ListCtrl.name, ListCtrl)
-    .controller(EditCtrl.name, EditCtrl)
-    .controller(NewCtrl.name, NewCtrl)
     .config(Routes)
     .name;
